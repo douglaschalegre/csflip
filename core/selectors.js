97e04a84a9bfe404a45e1bc6ec212e98178b4703
douglaschalegre <douglas.chalegre@gmail.com>
2025-10-19 13:13:04 -0300

    feat: Add central selector constants for CSFlip components

---
+++ core/selectors.js
@@ -0,0 +1,13 @@
+(function(){
+  // Central selector constants
+  const SELECTORS = {
+    PRICE_ROW: '.price-row',
+    PRICE_CELL: '.price',
+    DIALOG: '.mat-mdc-dialog-container, .mat-dialog-container',
+    ITEM_NAME_COMPONENT: 'app-item-name',
+    SALES_TABLE_CELL: 'td.mat-column-price .price',
+    FLIP_BOX: '.flip-helper-box',
+    TABLE_GENERIC: 'table, .mat-mdc-table'
+  };
+  window.CSFlipSelectors = SELECTORS;
+})();
